5f61ae9a1ba64b57217f3d44e072953c3f6f854d
Rudy5525 <laljacek@gmail.com>
2023-12-18 18:39:01 +0100

    test change

--- CNN/test.py
+++ CNN/test.py
@@ -17,17 +17,3 @@ plot(ADHD_DATA, patient, channel)
 plot(ADHD_FILTERED[bandwith], patient, channel)
 plt.show()
 
-# Przykładowy sygnał w dziedzinie czasu
-Fs = 128  # Częstotliwość próbkowania
-T = 1/Fs   # Okres próbkowania
-t = np.arange(0, ADHD_FILTERED[bandwith][patient][channel].shape[0])/Fs
-
-# Wykonaj FFT
-fft_result = np.fft.fft(ADHD_FILTERED[bandwith][patient][channel])
-frequencies = np.fft.fftfreq(len(fft_result), T)  # Wektor częstotliwości
-
-for i, data in enumerate(ADHD_FILTERED):
-    # Wykres widma częstotliwościowego dla danego pasma
-    plot_frequency_band(data[patient][channel], Fs, i)
-    
-plt.show()
