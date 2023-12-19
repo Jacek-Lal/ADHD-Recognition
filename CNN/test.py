8a1a201fdb9f531d65991769492b990bc6cb9344
matimusial <154259613+matimusial@users.noreply.github.com>
2023-12-19 21:20:10 +0100

    MAINTESTOWY.py

--- CNN/test.py
+++ CNN/test.py
@@ -1,29 +1,29 @@
 from eeg_read import *
 from config import *
 from plots import *
+'''
+import tensorflow as tf
 
-ADHD_DATA, CONTROL_DATA = readEEGRaw(EEG_DATA_PATH)
-ADHD_NORMALIZED, CONTROL_NORMALIZED = normalizeEEGData(ADHD_DATA, CONTROL_DATA)
-ADHD_FILTERED, CONTROL_FILTERED = filterEEGData(ADHD_NORMALIZED, CONTROL_NORMALIZED)
 
-bandwith = 0
-patient = 10
-channel = 10
+devices = tf.config.list_physical_devices()
 
-plot(ADHD_DATA, patient, channel)
-plot(ADHD_FILTERED[bandwith], patient, channel)
-plt.show()
+gpu_devices = [device for device in devices if 'GPU' in device.device_type]
 
-# Przykładowy sygnał w dziedzinie czasu
-T = 1/FREQ   # Okres próbkowania
-t = np.arange(0, ADHD_FILTERED[bandwith][patient][channel].shape[0])/FREQ
-
-# Wykonaj FFT
-fft_result = np.fft.fft(ADHD_FILTERED[bandwith][patient][channel])
-frequencies = np.fft.fftfreq(len(fft_result), T)  # Wektor częstotliwości
-
-for i, data in enumerate(ADHD_FILTERED):
-    # Wykres widma częstotliwościowego dla danego pasma
-    plot_frequency_band(data[patient][channel], i)
+if len(gpu_devices) > 0:
+    print("TensorFlow używa karty graficznej do obliczeń.")
+else:
+    print("TensorFlow używa CPU lub nie ma dostępnej karty graficznej.")
     
-plt.show()
+'''
+
+if __name__ == '__main__':
+    ADHD_DATA, CONTROL_DATA = readEEGRaw(EEG_DATA_PATH)
+    ADHD_NORMALIZED, CONTROL_NORMALIZED = normalizeEEGData(ADHD_DATA, CONTROL_DATA)
+
+    for i in range(len(CUTOFFS)-1):
+
+        ADHD_FILTERED, CONTROL_FILTERED = filterEEGData(ADHD_NORMALIZED, CONTROL_NORMALIZED, i)
+
+        plot_frequency_band(ADHD_FILTERED[0][0], i)
+
+    plt.show()
