0d619e8c9b8356a3358370bd91f0031b0a8c2856
matimusial <154259613+matimusial@users.noreply.github.com>
2023-12-19 20:40:00 +0100

    Update plots.py

--- CNN/plots.py
+++ CNN/plots.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from config import CUTOFFS, FREQ
+from config import CUTOFFS, FS
+
 def plot(data, patient_numer, channel_number):
-    t = np.arange(0, data[patient_numer][channel_number].shape[0])/FREQ
+    t = np.arange(0, data[patient_numer][channel_number].shape[0])/FS
     plt.plot(t, data[patient_numer][channel_number])
-    print(f"Czas dla kanału: {t[-1]} s.")
+    print(f"Czas dla kanału: {t[-1]:.3f} s.")
     print(f"Ilość próbek: {data[patient_numer][channel_number].shape}")
 
 def plot_frequency_band(data, band_number):
-    # Transformata Fouriera
-    frequencies = np.fft.fftfreq(len(data), d=1/FREQ)
+
+    frequencies = np.fft.fftfreq(len(data), d=1/FS)
     fft_values = np.fft.fft(data)
     magnitude_spectrum = np.abs(fft_values)
 
-    # Wykres widma częstotliwościowego
     plt.plot(frequencies, magnitude_spectrum, label=f'{CUTOFFS[band_number]} Hz')
     plt.title('Widmo częstotliwościowe')
     plt.xlabel('Częstotliwość (Hz)')
