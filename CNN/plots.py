5506a1740bc80c05e58e26fc46ad53a2fc07b6ca
matimusial <matimusial5@gmail.com>
2023-12-20 22:57:24 +0100

    xd

--- CNN/plots.py
+++ CNN/plots.py
@@ -14,7 +14,7 @@ def plot(data, patient_number, channel_number):
     plt.title('Wykres sygnału EEG')
     plt.legend()
 
-def plot_frequency_band(data, band_number):
+def plot_frequency_band(data, band_number = 2):
     #data = wektor pojedynczy
 
     frequencies = np.fft.fftfreq(len(data), d=1/FS)
--- CNN/test.py
+++ CNN/test.py
@@ -1,10 +1,10 @@
 import numpy as np
-
 from eeg_read import *
 from config import *
 from plots import *
 import matplotlib.pyplot as plt
 import copy
+from eeg_filter import *
 
 
 
@@ -12,5 +12,6 @@ ADHD_DATA, CONTROL_DATA = readEEGRaw(EEG_DATA_PATH)
 
 ADHD_FILTERED, CONTROL_FILTERED = filterEEGData(ADHD_DATA, CONTROL_DATA)
 
-plot(ADHD_FILTERED, 5, 6)
+plot_frequency_band(ADHD_DATA[0][5])
+
 plt.show()
