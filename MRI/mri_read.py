a761a50eb5b545ca38b8697bccb65b2643cecb56
Rudy5525 <61835186+Rudy5525@users.noreply.github.com>
2023-12-23 16:20:10 +0100

    mri_read changed

--- MRI/mri_read.py
+++ MRI/mri_read.py
@@ -1,47 +1,58 @@
 import os
+import time
 import nibabel as nib
-import numpy as np
 
-MRI_DATA_PATH = './MRI'
+MRI_DATA_PATH = r"MRI\files"
 
 
 def read_nii_file(file_path):
     img = nib.load(file_path)
     data = img.get_fdata()
     return data
-def read_MRI_file(root_folder):
+
+def getMRIData(root_folder_name):
     tasks = [
-        'task-SLD',
-        'task-SLI',
-        'task-SSD',
-        'task-SSI',
-        'task-VLD',
-        'task-VLI',
-        'task-VSD',
-        'task-VSI',
+        'SLD',
+        'SLI',
+        'SSD',
+        'SSI',
+        'VLD',
+        'VLI',
+        'VSD',
+        'VSI',
     ]
 
-    data_dict = {}
+    data_dict = {task: [] for task in tasks}
+    
+    for sub_num in range(1, 80):
+        
+        t1 = time.time()
+        
+        sub_folder_name = f'sub-{sub_num:02d}'
+        sub_folder_path = os.path.join(root_folder_name, sub_folder_name, 'ses-T1', 'func')
+        
+        if not os.path.exists(sub_folder_path): 
+            print("Path does not exist")
+            continue
 
-    for task in tasks:
-        task_data = {}
-        for sub_num in range(1, 80):
-            sub_folder = f'sub-{sub_num:02d}'
-            sub_path = os.path.join(root_folder, sub_folder, 'ses-T1', 'func')
-            if os.path.exists(sub_path):
-                nii_files = [f for f in os.listdir(sub_path) if f.startswith(f'{sub_folder}_ses-T1_{task}_bold.nii.gz')]
-                if nii_files:
-                    task_data[sub_folder] = []
-                    for nii_file in nii_files:
-                        nii_path = os.path.join(sub_path, nii_file)
-                        nii_data = read_nii_file(nii_path)
-                        task_data[sub_folder].append(np.array(nii_data))
-
-        data_dict[task] = task_data
-
+        nii_files = [f for f in os.listdir(sub_folder_path) if f.endswith(f"_bold.nii.gz")]
+        
+        if len(nii_files) == 0: 
+            print("There are no .nii files in this folder")
+            continue
+        
+        for file in nii_files:
+            nii_path = os.path.join(sub_folder_path, file)
+            nii_file_data = read_nii_file(nii_path)
+            task = next(part.split('-')[1] for part in nii_path.split("_") if part.startswith('task-'))
+            data_dict[task].append(nii_file_data)
+        
+        t2 = time.time()    
+        print(f"Tasks from sub {sub_num} added in {round(t2-t1, 2)} s")
+            
     return data_dict
 
 
-MRI_data = read_MRI_file(MRI_DATA_PATH)
+MRI_data = getMRIData(MRI_DATA_PATH)
 print(MRI_data)
 
