3079e359ba0d7574a1ceb3d01a9c19ac88f5ccd1
matimusial <matimusial5@gmail.com>
2023-12-27 18:44:37 +0100

    jacka kod

--- MRI/mri_read.py
+++ MRI/mri_read.py
@@ -3,11 +3,13 @@ import nibabel as nib
 import csv
 from config import *
 
+
 def read_nii_file(file_path):
     img = nib.load(file_path)
     data = img.get_fdata()
 
-    return data
+    return img
+
 
 def getAdhdLabels(file_path):
     data = []
@@ -18,40 +20,38 @@ def getAdhdLabels(file_path):
         for row in reader:
             hasAdhd = int(row[5])
             data.append(hasAdhd)
-    
+
     return data
 
-def getTaskMRI(task, adhd_labels):
-    
-    adhd_diagnosis = getAdhdLabels(PATIENTS_DATA_PATH)
+
+def getTaskMRI(task, adhd_labels, patientsNumber):
     data = []
 
-    for sub_num in range(1,len(adhd_diagnosis) + 1):
-        
+    for sub_num in range(1, patientsNumber + 1):
+
         hasAdhd = adhd_labels[sub_num - 1]
-        
+
         sub_folder_name = f'sub-{sub_num:02d}'
         sub_folder_path = os.path.join(MRI_DATA_PATH, sub_folder_name, 'ses-T1', 'func')
-        
-        if not os.path.exists(sub_folder_path): 
+
+        if not os.path.exists(sub_folder_path):
             print("Path does not exist")
             continue
-        
+
         file = None
         for f in os.listdir(sub_folder_path):
             if f.endswith(f"{task}_bold.nii.gz"):
                 file = f
-           
-        if file == None: 
+
+        if file == None:
             print(f"There is no task {task} in this folder")
             continue
-        
+
         nii_path = os.path.join(sub_folder_path, file)
         nii_file_data = read_nii_file(nii_path)
 
         patient_data = {"data": nii_file_data, "hasAdhd": hasAdhd}
-        
-        data.append(patient_data)
-            
-    return data
 
+        data.append(patient_data)
+
+    return data
