f06d7e3bd501c43896827402713243f274077a7d
matimusial <matimusial5@gmail.com>
2023-12-27 12:49:44 +0100

    push

--- EEG/PREDICT/eeg_read.py
+++ EEG/PREDICT/eeg_read.py
@@ -1,5 +1,9 @@
 import os
 from scipy.io import loadmat
+import math
+import numpy as np
+
+from EEG.config import *
     
 def readEEGRaw(path):
     mat_data = loadmat(path, mat_dtype=True)
@@ -7,3 +11,15 @@ def readEEGRaw(path):
     file, _ = os.path.splitext(os.path.basename(path))
 
     return mat_data[file].T
+
+def frameDATA(DATA):
+
+    DATA_range = (math.floor(DATA.shape[1] / EEG_SIGNAL_FRAME_SIZE))
+
+    DATA_framed = np.zeros((DATA_range, DATA.shape[0], EEG_SIGNAL_FRAME_SIZE))
+
+    for i in range(DATA_range):
+        DATA_framed[i, :, :] = DATA[:, i * EEG_SIGNAL_FRAME_SIZE: (i + 1) * EEG_SIGNAL_FRAME_SIZE]
+
+
+    return np.reshape(DATA_framed,(DATA_framed.shape[0],DATA_framed.shape[1],DATA_framed.shape[2],1))
