5af56c4a12f5af5cf2ead83d9ddc30c8691e3e95
Rudy5525 <61835186+Rudy5525@users.noreply.github.com>
2023-12-27 18:36:18 +0100

    mri plot

---
+++ MRI/mri_plot.py
@@ -0,0 +1,15 @@
+from nilearn import plotting
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_mri(img):
+  for i in range(img.shape[-1]):
+      volume_to_visualize = img.slicer[..., i]
+      colors = np.ones(volume_to_visualize.shape, dtype=bool)
+      
+      plotting.plot_epi(volume_to_visualize)
+      plotting.show()
+      plt.close()
+
+# Display the plot
