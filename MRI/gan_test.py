f8e6b18a21425bfe745fb11d8d722092c570642d
Rudy5525 <61835186+Rudy5525@users.noreply.github.com>
2023-12-28 19:46:27 +0100

    jako tako odpalajcy sie gan

--- MRI/gan_test.py
+++ MRI/gan_test.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 import pickle
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 from tensorflow import keras 
 from keras import layers, models
 from mri_read import *
@@ -22,96 +23,119 @@ def load_images_from_pickle(file_path):
             labels.append(patient['hasAdhd'])
 
     return np.array(images), np.array(labels)
+
 # Funkcja generująca losowy szum jako dane wejściowe dla generatora
 def generate_noise(batch_size, noise_dim):
     return np.random.normal(0, 1, size=(batch_size, noise_dim))
 
 # Funkcja tworząca generator
-def build_generator(noise_dim, output_dim):
+def normalize(data):
+    min = np.min(data)
+    max = np.max(data)
+    return (data-min)/(max-min)
+
+# Zakładając, że obrazy MRI są przechowywane w tablicy NumPy o nazwie 'mri_images'
+# o wymiarach (num_samples, 128, 120, 32)
+
+# Generator Model
+def build_generator(latent_dim):
     model = models.Sequential()
-    model.add(layers.Dense(128, input_dim=noise_dim, activation='relu'))
+    model.add(layers.Dense(128 * 120 * 32, input_dim=latent_dim))
+    model.add(layers.Reshape((128, 120, 32, 1)))
+    model.add(layers.Conv3D(128, (4, 4, 4), strides=(2, 2, 2), padding='same'))
     model.add(layers.BatchNormalization())
-    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Conv3DTranspose(64, (4, 4, 4), strides=(2, 2, 2), padding='same'))
     model.add(layers.BatchNormalization())
-    model.add(layers.Dense(np.prod(output_dim), activation='sigmoid'))
-    model.add(layers.Reshape(output_dim))  # Dodaj warstwę Reshape
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Conv3DTranspose(1, (4, 4, 4), activation='sigmoid', padding='same'))
+    model.summary()
     return model
 
-# Funkcja tworząca dyskryminator
-def build_discriminator(input_dim):
+# Discriminator Model
+def build_discriminator(img_shape):
     model = models.Sequential()
-    model.add(layers.Flatten(input_shape=input_dim))
-    model.add(layers.Dense(256, activation='relu'))
-    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Conv3D(64, (4, 4, 4), strides=(2, 2, 2), padding='same', input_shape=[128,120,32,1]))
+    
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Conv3D(128, (4, 4, 4), strides=(2, 2, 2), padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU(alpha=0.2))
+    model.add(layers.Flatten())
     model.add(layers.Dense(1, activation='sigmoid'))
+    model.summary()
     return model
 
-# Funkcja tworząca model GAN
+# Combined Model (Generator + Discriminator)
 def build_gan(generator, discriminator):
     discriminator.trainable = False
     model = models.Sequential()
     model.add(generator)
     model.add(discriminator)
+    
     return model
 
-# Parametry
-noise_dim = 100
-image_dim = (128, 120, 32, 1)  # Rozmiar obrazu po dodaniu warstwy Reshape
-batch_size = 64
-epochs = 1500
+# Konfiguracja GAN
+latent_dim = 100
+img_shape = (128, 120, 32, 1)
+generator = build_generator(latent_dim)
+discriminator = build_discriminator(img_shape)
 
-# Wczytanie danych z pliku .pkl
-data_path = 'lista.pkl'
-X_data, y_data = load_images_from_pickle(data_path)
-print(X_data.shape, y_data.shape)
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.20, shuffle=True)
-
-# Tworzenie i kompilacja modeli
-discriminator = build_discriminator(image_dim)
+#print(generator.output_shape)
+#print(discriminator.input_shape)
+# Kompilacja dyskryminatora
 discriminator.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+discriminator.trainable = False
 
-generator = build_generator(noise_dim, image_dim)
-generator.compile(optimizer='adam', loss='binary_crossentropy')
-
+# Kompilacja GAN
 gan = build_gan(generator, discriminator)
 gan.compile(optimizer='adam', loss='binary_crossentropy')
 
-# Pętla ucząca
-for epoch in range(epochs):
-    # Trening dyskryminatora
-    idx = np.random.randint(0, len(X_data), batch_size)
-    real_images = np.array(X_data)[idx]
-    labels_real = np.ones((batch_size, 1))
+# Trenowanie GAN
+epochs = 10
+batch_size = 32
+X_data, y_data = load_images_from_pickle('lista.pkl')
+mri_images = normalize(X_data)
 
-    noise = generate_noise(batch_size, noise_dim)
+for epoch in range(epochs):
+    print(1)
+    # Generowanie szumu z rozkładu normalnego jako wejście do generatora
+    # Generowanie szumu z rozkładu normalnego jako wejście do generatora
+    noise = np.random.normal(0, 1, (batch_size, latent_dim))
     generated_images = generator.predict(noise)
+    print(2)
+
+    # Losowy wybór rzeczywistych obrazów z danych wejściowych
+    idx = np.random.randint(0, mri_images.shape[0], batch_size)
+    real_images = mri_images[idx]
+
+    print(3)
+    # Etykiety dla rzeczywistych i wygenerowanych obrazów
+    labels_real = np.ones((batch_size, 1))
     labels_fake = np.zeros((batch_size, 1))
 
+    print(4)
+    
+    # Trenowanie dyskryminatora na rzeczywistych i wygenerowanych danych
     d_loss_real = discriminator.train_on_batch(real_images, labels_real)
     d_loss_fake = discriminator.train_on_batch(generated_images, labels_fake)
-    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+    print(5)
 
-    # Trening generatora
-    noise = generate_noise(batch_size, noise_dim)
+    # Obliczanie straty dyskryminatora
+    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+    print(6)
+
+    # Generowanie nowego szumu dla wejścia do GAN
+    noise = np.random.normal(0, 1, (batch_size, latent_dim))
     labels_gan = np.ones((batch_size, 1))
 
+    print(7)
+    # Trenowanie GAN na wygenerowanych danych
     g_loss = gan.train_on_batch(noise, labels_gan)
+    print(8)
 
-    # Wydruk statystyk co kilka epok
-    if epoch % 100 == 0:
-        print(f"Epoch {epoch}, D Loss: {d_loss[0]}, G Loss: {g_loss}")
+    # Wydrukowanie straty co pewną liczbę epok
+    print(f"Epoch {epoch}, D Loss: {d_loss[0]}, G Loss: {g_loss}")
 
-'''
-# Generowanie przykładowego obrazka po zakończeniu treningu
-sample_noise = generate_noise(1, noise_dim)
-generated_sample = generator.predict(sample_noise).reshape(128, 120, 32)
-
-# Wyświetlenie przykładowego obrazka
-plt.imshow(generated_sample[:, :, 16], cmap='gray')  # Wybierz warstwę 16 z wymiaru 32
-plt.show()
-
-# Zapisanie wygenerowanego obrazka w formacie NIfTI
-output_path = 'generated_image.nii.gz'
-nifti_image = nib.Nifti1Image(generated_sample, np.eye(4))
-nib.save(nifti_image, output_path)
-'''
+# Generowanie syntetycznych obrazów za pomocą wyuczonego generatora
+synthetic_images = generator.predict(np.random.normal(0, 1, (1, latent_dim)))
--- MRI/mri_load_test.py
+++ MRI/mri_load_test.py
@@ -7,7 +7,6 @@ from mri_plot import *
 
 #adhdLabels = getAdhdLabels(PATIENTS_DATA_PATH)
 #patientsNumber = 11
-
 # data = getTaskMRI(TASKS[0], adhdLabels, patientsNumber) # List[{data: np.array, hasAdhd: int}]
 # print(f"Shape: {data[0]['data'].shape}, {data[1]['data'].shape}, {data[2]['data'].shape}")
 # print(f"Size of data: {asizeof.asizeof(data) / (1024.0**3):.2f} GB")
@@ -16,9 +15,8 @@ from mri_plot import *
 with open('lista.pkl', 'rb') as f:
     data = pickle.load(f)    
 
-plot_mri(data[0]['data'])
+#plot_mri(data[0]['data'])
 print(getData(data[0]['data']))
-
-del data
-gc.collect()
+#del data
+#gc.collect()
 
